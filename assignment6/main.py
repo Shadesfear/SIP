77666b994e3afcafef123556a773491b8009fd87
Unknown <Blaedelblaedel@gmail.com>
2020-03-12 11:13:08 +0100

    Create main file for group assingment 6

---
+++ assignment6/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+This file is is the main file for the group assingment 6
+
+"""
+
+
+
+
+
+
+def main():
+
+    print("done")
+    
+
+
+if __name__ == "__main__":
+    main()
