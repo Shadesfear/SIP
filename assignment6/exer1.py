e9be62a83f2a134885ed86187666cc3764ed293f
Unknown <Blaedelblaedel@gmail.com>
2020-03-12 11:35:01 +0100

    create function exer11, exer 12 and exer13 with deliverables

--- assignment6/exer1.py
+++ assignment6/exer1.py
@@ -6,7 +6,54 @@ Created on Thu Mar 12 11:21:11 2020
 This file holds the solution to section 1 for assingment 6 SIP
 """
 
+from  skimage.feature import canny
+import matplotlib.pyplot as plt
+
+
+testImageFolder = "./Week 6/"
+
+saveImageFolder = "./imageResults/"
+
+
+
+def exer11():
+    """
+    Deliverables: 
+        Include an illustration showing the results of the different set-
+        tings and explain what the effect is of 
+        each of the parameters based on these results.
+    """    
+    
+    pass
+
+
+img = plt.imread(testImageFolder + "hand.tiff")
+## plots BGR colours should be fixed. 
+plt.imsave(saveImageFolder + "hand.tiff", img)
 
 
 
 
+def exer12():
+    """
+    Deliverables: 
+        Include an illustration showing the results of the different set- 
+        tings and explain what the effect is of 
+        each of the parameters based on these results.
+    """
+
+    pass
+
+
+def exer13():
+    """
+    Deliverables: 
+        Include in the report the code for your function. 
+        Apply this function to the modelhouses.png image and 
+        create a figure of the resulting corner points overlaid 
+        on the modelhouses.png image. 
+        Remember to indicate your choice of parameter settings 
+        in the caption of the figure.
+    """
+    
+    pass
