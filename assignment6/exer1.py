a33ab0366034cb1b4524215e29e0553e7899681e
Unknown <Blaedelblaedel@gmail.com>
2020-03-12 11:41:31 +0100

    setup main & exer1 to take arguemt; testimagesfolder & savedImagefolder

--- assignment6/exer1.py
+++ assignment6/exer1.py
@@ -10,31 +10,30 @@ from  skimage.feature import canny
 import matplotlib.pyplot as plt
 
 
-testImageFolder = "./Week 6/"
-
-saveImageFolder = "./imageResults/"
 
 
-
-def exer11():
+def exer11(testImageFolder,saveImageFolder):
     """
     Deliverables: 
         Include an illustration showing the results of the different set-
         tings and explain what the effect is of 
         each of the parameters based on these results.
-    """    
+    """  
+    
+    # small test to see if the folders where correct
+    img = plt.imread(testImageFolder + "hand.tiff")
+    ## plots BGR colours should be fixed. 
+    plt.imsave(saveImageFolder + "hand.tiff", img)
+    
+    
+
     
     pass
 
 
-img = plt.imread(testImageFolder + "hand.tiff")
-## plots BGR colours should be fixed. 
-plt.imsave(saveImageFolder + "hand.tiff", img)
 
 
-
-
-def exer12():
+def exer12(testImageFolder,saveImageFolder):
     """
     Deliverables: 
         Include an illustration showing the results of the different set- 
@@ -45,7 +44,7 @@ def exer12():
     pass
 
 
-def exer13():
+def exer13(testImageFolder,saveImageFolder):
     """
     Deliverables: 
         Include in the report the code for your function. 
@@ -57,3 +56,18 @@ def exer13():
     """
     
     pass
+
+
+def main():
+    
+    testImageFolder = "./Week 6/"
+
+    saveImageFolder = "./imageResults/"
+    
+    exer11(testImageFolder,saveImageFolder)
+    exer12(testImageFolder,saveImageFolder)
+    exer13(testImageFolder,saveImageFolder)
+
+if __name__ == "__main__":
+    
+    main()
--- assignment6/main.py
+++ assignment6/main.py
@@ -6,12 +6,21 @@ This file is is the main file for the group assingment 6
 
 """
 
-
+import exer1
 
 
 
 
 def main():
+    
+    testImageFolder = "./Week 6/"
+
+    saveImageFolder = "./imageResults/"
+    
+    exer1.exer11(testImageFolder,saveImageFolder)
+    exer1.exer12(testImageFolder,saveImageFolder)    
+    exer1.exer13(testImageFolder,saveImageFolder)    
+    
 
     print("done")
     
