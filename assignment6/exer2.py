9e0a2db5cda0c354fc9e3ea73bb85acd1980117f
Christopher <chris@thecarmans.com>
2020-03-17 23:43:43 +0100

    22222

--- assignment6/exer2.py
+++ assignment6/exer2.py
@@ -9,9 +9,12 @@ This file holds the solution to section 1 for assingment 6 SIP
 import numpy as np
 from  skimage.feature import canny
 import matplotlib.pyplot as plt
-from skimage import io
+from skimage import io, filters
 from scipy.signal.windows import gaussian
-from scipy.signal import convolve2d
+from scipy.signal import convolve2d, convolve
+
+from skimage.feature import blob_dog, blob_log, blob_doh
+from scipy.ndimage.filters import laplace, gaussian_filter, sobel
 from tqdm import tqdm
 
 
@@ -70,10 +73,18 @@ def exer23iii(testImageFolder,saveImageFolder):
          Confirm your result in Python by plotting H(0,0,τ) 
          as a function of τ using the expression from 2.3.i.
     """
+    pi=22/7
+    sigma = 1
+    h = lambda t: -1/(pi**2*(sigma**2+t**2)**2)
+    x = np.linspace(0, 10, 10000)
 
-    pass
+    plt.plot(x, h(x))
+    plt.xlabel('t')
+    plt.ylabel('H')
+    # plt.savefig(saveImageFolder + 'exer23iii.pdf')
 
-
+    plt.show()
+    plt.close()
 def exer23iv(testImageFolder,saveImageFolder):
     """
     Deliverables: 
@@ -82,9 +93,46 @@ def exer23iv(testImageFolder,saveImageFolder):
         What image structure does maxima of eq. (6) represent, 
         and what image structure does minima represent?
     """
-    
-    pass
 
+    img = io.imread(testImageFolder + "sunflower.tiff")
+    img = img/255
+    sigs = np.linspace(0, 30, 10)
+    img_pyramide = [gaussian_filter(img, sig) for sig in sigs]
+    img_pyramide_np = np.stack(img_pyramide, axis = 0)
+
+    def detect_all_the_blobs(image, sigma):
+        co_ordinates = [] #to store co ordinates
+        (s,h,w) = image.shape
+        for i in range(1,h):
+            for j in range(1,w):
+                slice_img = image[:,i-1:i+2,j-1:j+2]
+                result = np.amax(slice_img)
+                if result >= 0.1:
+                    z,x,y = np.unravel_index(slice_img.argmax(),slice_img.shape)
+                    co_ordinates.append((i+x-1,j+y-1,sigma[z])) #finding co-rdinates
+        return co_ordinates
+
+    coordinates = list(set(detect_all_the_blobs(laplace(img_pyramide_np), sigs)))
+
+    blobs_log = blob_log(np.max(img)-img, max_sigma = 30, num_sigma=10, threshold=.03)
+    blobs_log[:,2] = blobs_log[:,2] * np.sqrt(2)
+
+    fig, axes = plt.subplots()
+    axes.imshow(img, cmap='gray')
+    for blob in blobs_log:
+        y,x,r = blob
+        c = plt.Circle((x, y), r, color='red' ,linewidth=1.3, fill=False)
+        axes.add_patch(c)
+    plt.savefig(saveImageFolder + 'blobs_scipy.pdf')
+
+
+    fig, axes = plt.subplots()
+    axes.imshow(img, cmap='gray')
+    for blob in coordinates:
+        y,x,r = blob
+        c = plt.Circle((x, y), r, color='blue' ,linewidth=1.3, fill=False)
+        axes.add_patch(c)
+    plt.savefig(saveImageFolder + 'blobs_own.pdf')
 
 def exer24(testImageFolder,saveImageFolder):
     """
@@ -125,12 +173,12 @@ def main():
 
     saveImageFolder = "./exer2Images/"
     
-    exer21(testImageFolder,saveImageFolder)
-    exer23iii(testImageFolder,saveImageFolder)
+    # exer21(testImageFolder,saveImageFolder)
+    # exer23iii(testImageFolder,saveImageFolder)
     exer23iv(testImageFolder,saveImageFolder)
-    exer24(testImageFolder,saveImageFolder)
-    exer25iii(testImageFolder,saveImageFolder)
-    exer25iv(testImageFolder,saveImageFolder)
+    # exer24(testImageFolder,saveImageFolder)
+    # exer25iii(testImageFolder,saveImageFolder)
+    # exer25iv(testImageFolder,saveImageFolder)
 
 if __name__ == "__main__":
     
