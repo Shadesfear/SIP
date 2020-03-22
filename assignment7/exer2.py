48d29e042d4bcbb7f752c91e8fe1046f0cf265a3
Unknown <Blaedelblaedel@gmail.com>
2020-03-22 20:01:21 +0100

    create image folder and create exer2 file

---
+++ assignment7/exer2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+The file to answer question two in group assingment 7
+"""
+
+
+
+def exer2():
+    
+    print("done")
+    
+    
+    
+if __name__ == "__main__":
+    exer2()
