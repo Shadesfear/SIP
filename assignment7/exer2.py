542d8ce84f05c22aa4ad85744a98635c90bbbd1c
Rune Søndergaard <rvsoendergaard@gmail.com>
2020-03-23 22:06:00 +0100

    work work

--- assignment7/exer2.py
+++ assignment7/exer2.py
@@ -10,8 +10,6 @@ import matplotlib.pyplot as plt
 
 def makePatches(img,shape=(29,29)):
     
-    M,N = shape
-    
     tiles = [img[x:x+shape[0],y:y+shape[1]] for x in range(0,img.shape[0],1) 
              for y in range(0,img.shape[1],1)]
     for i,val in enumerate(tiles):
@@ -19,25 +17,65 @@ def makePatches(img,shape=(29,29)):
             zero_shape = np.zeros(shape)
             zero_shape[:val.shape[0],:val.shape[1]] = tiles[i]
             tiles[i] = zero_shape
-        tiles[i] = np.reshape(tiles[i]/255,(1,1,29,29))
+        tiles[i] = np.reshape(tiles[i]/255,(1,1,shape[0],shape[1]))
     return tiles
+
+# def dice(im1, im2):
+
+#     im1 = np.asarray(im1).astype(np.bool)
+#     im2 = np.asarray(im2).astype(np.bool)
+
+#     if im1.shape != im2.shape:
+#         raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+#     im_sum = im1.sum() + im2.sum()
+
+#     # Compute Dice coefficient
+#     intersection = np.logical_and(im1, im2)
+
+#     return 2. * intersection.sum() / im_sum
+
+def dice(predictions, thruth):
+    r,c = thruth.shape
+    
+    intersection = np.equal(predictions, thruth)
+    intersect = np.sum(intersection)
+    
+    dice = 2 * intersect/ (r*c*2)
+
+    return dice
+
 
 def exer2_3():
     model = keras_own()
     
     test_img = io.imread("./Week_7_export/test_images/image/1003_3_image.png")
     
-    patches = makePatches(test_img) 
+    # patches = makePatches(test_img) 
     
-    pred = np.empty((0,1))
-    count = 0
-    for i in patches:
+    # pred = np.empty((0,1))
+    # count = 0
+    # for i in patches:
         
-        pred = np.append(pred,np.argmax(model.predict(i)))
-        count += 1
-        print(count)
-    np.reshape(pred,(256,256))
-    pass
+    #     pred = np.append(pred,np.argmax(model.predict(i)))
+    #     count += 1
+    #     print(count)
+    # pred = np.reshape(pred,(256,256))
     
+    pred = np.load("exer23.npy")
+    
+    plt.imshow(pred)
+    plt.colorbar()
+    plt.savefig("./images/exer23.png",dpi=500,bbox_inches="tight")
+    plt.close()
+
+
+def exer2_4():
+    groundTruth = io.imread("./Week_7_export/test_images/seg/1003_3_seg.png")
+    seg_own = np.load("exer23.npy")
+    
+    dice1 = dice(seg_own,groundTruth)
+    pass
+
 if __name__ == "__main__":
-    exer2_3()
+    exer2_4()
